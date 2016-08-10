df9802c669782b3ab7dd0622b3fc279aed23581e
Caspar Nagel <casparen@me.com>
2016-08-10 14:15:36 -0400

    merging

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const twilio = require('twilio');
 const cronJob = require('cron').CronJob;
 const TWILIO_ACCOUNT_SID = process.env.TWILIO_ACCOUNT_SID;
 const TWILIO_AUTH_TOKEN = process.env.TWILIO_AUTH_TOKEN;
+const TWILIO_NUMBER = process.env.TWILIO_NUMBER
 const client = twilio(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN);
 const app = express();
 
