e341b8fc0cc3d7d32e2edbd38d2b4bd9851d44bd
Caspar Nagel <casparen@me.com>
2016-08-10 13:50:40 -0400

    working on message

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const twilio = require('twilio');
 const cronJob = require('cron').CronJob;
 const TWILIO_ACCOUNT_SID = process.env.TWILIO_ACCOUNT_SID
 const TWILIO_AUTH_TOKEN = process.env.TWILIO_AUTH_TOKEN
+const TWILIO_NUMBER = process.env.TWILIO_NUMBER
 const client = twilio(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN);
 const app = express();
 
@@ -16,6 +17,8 @@ const app = express();
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 app.use(cors());
+
+
 
 const userArr = [
     {Fname: "Harry", mNumber: "+19175172934", dob:"1203"},
@@ -37,7 +40,17 @@ const userArr = [
 ////////////////////////////////////////////////////////////birthday kid
 var job = new cronJob('* * * * * *', function (useArr) {
     let arr1 = userArr.filter(each => each.dob === format('MM', new Date()) + (parseInt(format('dd', new Date()), 10) + 1));
-    console.log(arr1);
+    if (arr1.length > 0) {
+      const arr2 = userArr.map(each => each.mNumber);
+      // console.log(arr2);
+      arr2.map(each => {
+        client.sms.messages.create({
+          to: each,
+          from: TWILIO_NUMBER,
+          body: `BIRTHDAY REMINDER: Tomorrow is ${arr1.Fname}'s birthday!'`
+        }, (err, data) => console.log(data.body))
+      })
+    }
 
     // for (let i = 0; i < userArr.length; i++) {
     //     if (userArr[i].dob == format('MM', new Date()) + (parseInt(format('dd', new Date()), 10) + 1)) {
